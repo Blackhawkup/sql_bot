94024bc3753aee4470fafa61c175e5418d4c2806
RedtRocks <aarav.dudeja3011@gmail.com>
2025-11-05 17:24:01 +0530

    login page alignment fix

--- src/components/LoginForm.jsx
+++ src/components/LoginForm.jsx
@@ -41,13 +41,13 @@ export default function LoginForm() {
           Username
         </label>
         <div className="relative">
-          <div className="absolute inset-y-0 left-0 pl-4 flex items-center pointer-events-none">
+          <div className="absolute inset-y-0 left-0 pl-5 flex items-center pointer-events-none z-10">
             <svg className="w-5 h-5 text-secondaryGray-700" fill="currentColor" viewBox="0 0 20 20">
               <path fillRule="evenodd" d="M10 9a3 3 0 100-6 3 3 0 000 6zm-7 9a7 7 0 1114 0H3z" clipRule="evenodd" />
             </svg>
           </div>
           <input 
-            className="input pl-12" 
+            className="input !pl-12" 
             value={username} 
             onChange={(e) => setUsername(e.target.value)} 
             placeholder="Enter your username" 
@@ -62,13 +62,13 @@ export default function LoginForm() {
           Password
         </label>
         <div className="relative">
-          <div className="absolute inset-y-0 left-0 pl-4 flex items-center pointer-events-none">
+          <div className="absolute inset-y-0 left-0 pl-5 flex items-center pointer-events-none z-10">
             <svg className="w-5 h-5 text-secondaryGray-700" fill="currentColor" viewBox="0 0 20 20">
               <path fillRule="evenodd" d="M5 9V7a5 5 0 0110 0v2a2 2 0 012 2v5a2 2 0 01-2 2H5a2 2 0 01-2-2v-5a2 2 0 012-2zm8-2v2H7V7a3 3 0 016 0z" clipRule="evenodd" />
             </svg>
           </div>
           <input 
-            className="input pl-12" 
+            className="input !pl-12" 
             type="password" 
             value={password} 
             onChange={(e) => setPassword(e.target.value)} 
